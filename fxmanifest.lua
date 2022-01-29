047e32510d276764e9395d088ffadbdd30830e35
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-29 14:22:38 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,6 @@ description 'apolo_weed'
 version '1.0.0'
 
 shared_script '@qb-core/shared.lua'
+-- shared_script '@qb-core/shared/items.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
