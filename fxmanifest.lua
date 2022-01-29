ecc1d249d4c745f8e0ea5d68cff98bf7dec218ac
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-29 14:21:23 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'apolo_weed'
+version '1.0.0'
+
+shared_script '@qb-core/shared.lua'
+server_script 'server/main.lua'
+client_script 'client/main.lua'
